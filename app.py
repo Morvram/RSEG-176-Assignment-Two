159988f85f45efb571b015cf0770a93c6dd0b492
J.R. Mandell <JRMhere@protonmail.ch>
2022-11-17 08:37:40 -0500

    login method in progress.
    known error: running app throws "cannot import name 'login_required' from 'helpers'. helpers package is supposed to contain a login_required function that aids with flask app. Tried installing with pip3 both 'helpers' and 'Flask-Login' and 'Flask-Helpers', no dice yet.

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, flash, render_template, send_file, request, session, url_for, flash, send_file
+from flask_session import Session
+from flask_uploads import UploadSet, configure_uploads
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 from documentConversion import convertDoc, removeWatermark
+from helpers import login_required
 
 application = app = Flask(__name__)
 application.config['SECRET_KEY'] = 'supersecretkey'
 application.config['UPLOAD_FOLDER'] = 'Static/Files'
+
+application.config['SESSION_FILE_DIR'] = mkdtemp()
+application.config['SESSION_PERMANENT'] = False
+application.config['SESSION_TYPE'] = 'filesystem'
+Session(application)
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
@@ -16,6 +24,7 @@ class UploadFileForm(FlaskForm):
 
 @application.route('/', methods=['GET',"POST"])
 @application.route('/home', methods=['GET',"POST"])
+#TODO require login.
 def home():
     form = UploadFileForm()
     if form.validate_on_submit():
@@ -31,6 +40,27 @@ def home():
         return send_file(outpath.replace(" ","_"))
         #return "File has been uploaded. <a href='/'>Return to Index </a>"
     return render_template('index.html', form=form)
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    #Forget any user_id
+    
+    session.clear()
+    
+    #User reached route by posting (submitting the login form):
+    if request.method == "POST":
+        #Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("Must provide username.")
+        #Ensure password was submitted
+        if not request.form.get("password"):
+            return apology("Must provide password.")
+        
+        
+            
+def apology(message):
+    flash(message)
+    return render_template("index.html", form=form)
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True, port=8080)
